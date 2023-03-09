6d3969e830d53b4ea663cdadefe5dfeb83ad2d71
FernandoLimas <nando_filipe@hotmail.com>
2023-03-09 14:36:51 -0300

    feat loading

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -38,7 +38,7 @@ const Home = () => {
           <button className='btn btn-dark'>Pesquisar</button>
         </form>
         <div>
-          {loading && <p>Carregando...</p>}
+          {/* {loading && <p>Carregando...</p>} */}
           {posts && posts.map((post) => (
               <PostDetail key={post.id} post={post}/>
             // <div>
